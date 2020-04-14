c07751c3b68c9aa120acdc000349b3cbe796e0f4
Matthew Malecki <mmalecki4@gmail.com>
2020-04-14 03:51:56 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,12 +14,12 @@ def link
   puts phrase
 end
 
-def all_phrases(the_method)
-  if the_method == mario
-    puts "It's-a me, Mario!"
-  elsif the_method == toadstool
-    puts 'Thank You Mario! But Our Princess Is In Another Castle!'
-  elsif the_method == link
-    puts "It's Dangerous To Go Alone! Take This."
-  else
-end
+#def all_phrases(the_method)
+  #if the_method == mario
+    #puts "It's-a me, Mario!"
+  #elsif the_method == toadstool
+    #puts 'Thank You Mario! But Our Princess Is In Another Castle!'
+  #elsif the_method == link
+    #puts "It's Dangerous To Go Alone! Take This."
+  #else
+#end
